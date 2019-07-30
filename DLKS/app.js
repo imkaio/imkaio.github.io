771443e2d576b9826f4c51eb4d3b11082fe2a5cd
Kaio Almeida <37541735+imkaio@users.noreply.github.com>
2019-07-30 10:16:43 -0300

    review
    up

---
+++ DLKS/app.js
@@ -0,0 +1,31 @@
+// Menu Mobile
+const navSlide = () => {
+    const burguer = document.querySelector('.burguer')
+    const nav = document.querySelector('ul.mobile')
+
+    burguer.addEventListener('click', () => {
+        nav.classList.toggle('nav-activate');
+    });
+}
+
+navSlide()
+
+// Menu Stick
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 0) {
+        document.querySelector("nav.menu").style.height = "60px";
+        document.querySelector("nav.menu").style.background = "#000";
+    } else {
+        document.querySelector("nav.menu").style.height = "115px";
+        document.querySelector("nav.menu").style.background = "linear-gradient(#000 0%, rgba(0, 0, 0, 0) 100%)";
+    }
+}
+
+// Barra de Frete
+function fecharFaixaFrete() {
+    const element = document.querySelector(".faixa__frete--gratis");
+    element.classList.add("fecharFaixa");
+}
+
